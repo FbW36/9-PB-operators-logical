34cf8df6430b3c81e1bc02186c70f89609e43933
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-07 14:56:56 +0200

    With comments

--- adrian/index.js
+++ adrian/index.js
@@ -14,19 +14,19 @@ let starsInSky = 2;
 
 //3.Check the result of whether:
 //a
-console.log(atoms > starsInSky && atoms > sandGrains); // false
+console.log(atoms > starsInSky && atoms > sandGrains); // false. The first part of the expression is false so the second part is not evaluated because the logical && operator is true only if all of its operands are true.
 
 //b
-console.log(atoms !== sandGrains); // true
+console.log(atoms !== sandGrains); // true. The value assigned to the variable atoms is different to the value assigned to the variable sandGrains.
 
 //c
-console.log(starsInSky < sandGrains || starsInSky > atoms); // true
+console.log(starsInSky < sandGrains || starsInSky > atoms); // true. It is true because we are using the OR operator, so if only one of its operand is true, the result is going to be true. (The second part is true)
 
 //d
-console.log(atoms === starsInSky || atoms !== sandGrains); //true
+console.log(atoms === starsInSky || atoms !== sandGrains); // true. It is true because we are using the OR operator, so if only one of its operand is true, the result is going to be true. (The second part is true).
 
 //e
-console.log(atoms >= 10 && sandGrains <= 10); //false
+console.log(atoms >= 10 && sandGrains <= 10); //false. The logical && operator is true only if all of its operands are true. (In this case both operands are false)
 
 //f
-console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100); //true
+console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100); // true. It is true because we are using the OR operator, so if only one of its operand is true, the result is going to be true. (The first part is true).
