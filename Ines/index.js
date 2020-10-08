b4542bc3cc797e0136a6d57febed6a18715585fc
Ines <inesfcnogueira@gmail.com>
2020-10-08 14:33:20 +0200

    Added requested changes

--- Ines/index.js
+++ Ines/index.js
@@ -5,6 +5,8 @@ console.log("1.a is", dogIsbetter && catIsbetter);
 
 console.log("1.b is", dogIsbetter || catIsbetter);
 
+console.log("1.c is", !(dogIsbetter && catIsbetter));
+
 let atoms = 1000;
 let sandGrains = 10;
 let starsInSky = 100;
@@ -12,13 +14,13 @@ let starsInSky = 100;
 console.log("2.a is", atoms > starsInSky && atoms > sandGrains);
 //both statements are true so it returns the last result, which is true
 
-console.log("2.b is", atoms != sandGrains);
+console.log("2.b is", atoms !== sandGrains);
 //the statement is true, they do not have the same value
 
 console.log("2.c is", starsInSky < sandGrains || starsInSky > atoms);
 //both statements are false so it returns the last statement which is false
 
-console.log("2.d is", atoms == starsInSky || atoms != sandGrains);
+console.log("2.d is", atoms == starsInSky || atoms !== sandGrains);
 // second statement is true so it returns the second one
 
 console.log("2.e is", atoms >= 10 && sandGrains <= 10);
