63f078b7bdb743d25849240db668423f603194a4
Amit833 <amitmajumder833@gmail.com>
2020-10-11 12:35:51 +0200

    amit,modified

--- amit/index.js
+++ amit/index.js
@@ -1,4 +1,4 @@
-gitlet dogIsBetter = true;
+let dogIsBetter = true;
 let catIsBetter = false;
 // 1. a,b,c
 console.log(dogIsBetter && catIsBetter);
@@ -12,19 +12,19 @@ let atoms = 30;
 let sandGrains = 20;
 let starsInSky = 10;
 // ans 3 a
-console.log("ans. a ==>", atoms > (sandGrains, starsInSky));
+console.log("q3a:", atoms > starsInSky && atoms > sandGrains);
 
 // ans 3 b
-console.log("ans. b ==>", atoms != sandGrains);
+console.log("q3b:", atoms !== sandGrains);
 
 // ans 3 c
-console.log("ans. c ==>", starsInSky < sandGrains || starsInSky > atoms);
+console.log("q3c:", starsInSky < sandGrains || starsInSky > atoms);
 
 // ans 3 d
-console.log("ans. d ==>", atoms == starsInSky || atoms != sandGrains);
+console.log("q3d:", atoms == starsInSky || atoms != sandGrains);
 
 // ans 3 e
-console.log("ans. e ==>", atoms >= 10 && sandGrains <= 10);
+console.log("q3e:", atoms >= 10 && sandGrains <= 10);
 
 // ans 3 f
-console.log("ans. f ==>", atoms * starsInSky < 100 || atoms * sandGrains > 100);
+console.log("q3f:", atoms * starsInSky < 100 || atoms * sandGrains > 100);
